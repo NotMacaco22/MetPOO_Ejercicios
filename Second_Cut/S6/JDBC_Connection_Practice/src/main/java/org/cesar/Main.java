7956979467446c3525538965c7832a4aadb883f8
César Silva <127228753+NotMacaco22@users.noreply.github.com>
2024-09-27 09:43:46 -0600

    Update Main.java

--- Second_Cut/S6/JDBC_Connection_Practice/src/main/java/org/cesar/Main.java
+++ Second_Cut/S6/JDBC_Connection_Practice/src/main/java/org/cesar/Main.java
@@ -1,8 +1,6 @@
 package org.cesar;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public Main() {
@@ -14,15 +12,27 @@ public class Main {
         DriverManager.drivers().forEach((driver) -> System.out.println(driver.toString()));
 
         try {
-            Connection c = DriverManager.getConnection("jdbc:postgresql://localhost/testing", "UserCredentials", "UserCredentialsButPassword");
-            System.out.println("Is Closed? - " + c.isClosed());
-            System.out.println("Product Name: " + c.getMetaData().getDatabaseProductName());
+            Connection c = DriverManager.getConnection("jdbc:postgresql://localhost/testing", "Credential_User", "Credential_Passwprd");
+            System.out.println("\nIs Closed? - " + c.isClosed());
+            System.out.println("Product Name: " + c.getMetaData().getDatabaseProductName() + "\n");
+
+            // Testing a query through java.sql with the help of the driver 
+            String query = "SELECT id, nombre FROM prueba";
+            Statement stmt = c.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            while (rs.next()) { // While more rows keep printing 
+                int id = rs.getInt("id");
+                String nombre = rs.getString("nombre");
+                System.out.println("ID: " + id + " | Nombre: " + nombre);
+            }
+
             c.close();
-            System.out.println("Is Closed? - " + c.isClosed());
-        } catch (SQLException sqlexception) {
+            System.out.println("\nIs Closed? - " + c.isClosed());
+
+        } catch (SQLException sqlexception) { // Exception if there's an error trying to connect to the databas
             SQLException e = sqlexception;
             System.out.println("There was an error trying to connect to the database.\n" + e.getMessage());
         }
-
     }
 }
